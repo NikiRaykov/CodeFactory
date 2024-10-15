b38be6057307343510105387d5bc006ccad13be5
NikiRaykov <nikiraiikov@gmail.com>
2024-10-15 17:54:42 +0200

    commit

---
+++ Java Script problems/Crystal Glazer/crystalGlazer.js
@@ -0,0 +1,12 @@
+const numberOfChildren = prompt("Enter the number of children: ");
+const name = prompt("Enter the name: ");
+const getLocation = ("Enter the location: ");
+const jobTitle = ("Enter the job title: ");
+
+function crystalGlazer(numberOfChildren, name, getLocation, jobTitle) {
+    console.log(`You will be a ${jobTitle} in ${getLocation}, and married to ${name} with ${numberOfChildren} kids.`)
+}
+
+
+crystalGlazer(numberOfChildren, name, getLocation, jobTitle);
+
